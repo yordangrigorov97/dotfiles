554562ccf576a121ccf0e77a164f69d991a5c8f5
Yordan Grigorov <yoreei.grigorov@gmail.com>
2020-12-03 22:58:37 +0100

    .bash_profile

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-rsync -rv --exclude=README.md --exclude=install.sh --exclude=.git ./ ~
+rsync -rv --exclude=README.md --exclude=install.sh --exclude=.git $(dirname "$0")/ ~
+# https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 
